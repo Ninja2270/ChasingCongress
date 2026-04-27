8f44fcba6db027c50a9a7a8940e491d09ae9e09d
Ninja2270 <Monts2fj@gmail.com>
2026-04-27 15:49:35 -0400

    Timer fixes

--- My project/Assets/_Scripts/Timer.cs
+++ My project/Assets/_Scripts/Timer.cs
@@ -6,28 +6,50 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
+    [SerializeField] bool timeIsRunning = true;
 
     // Update is called once per frame
+    private void Awake()
+    {
+        ResetTimer();
+        timeIsRunning = true;
+        timerText.color = Color.white;
+    }
     void Update()
     {
-        if(remainingTime > 0)
+        if(timeIsRunning && remainingTime > 0)
         {
             remainingTime -= Time.deltaTime;
+            
         }
         else if (remainingTime < 0)
         {
             remainingTime = 0;
             timerText.color = Color.red;
+           
+           
+           
         }
         if(remainingTime == 0)
         {
-            SceneManager.LoadScene(0);
+          
+            
+            timeIsRunning = false;
+            
+            SceneManager.LoadSceneAsync("TitleScreen");
+            ResetTimer();
         }
+        
 
 
           
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+    void ResetTimer()
+    {
+        remainingTime = 10;
+        timeIsRunning = false;
     }
 }
